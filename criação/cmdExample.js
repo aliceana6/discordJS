e0d03341d9246c28e468ff8fef8534f28eec42de
Enersh <56371070+aliceana6@users.noreply.github.com>
2019-10-09 22:35:43 -0300

    Create cmdExample.js

---
+++ criação/cmdExample.js
@@ -0,0 +1,12 @@
+module.exports = {
+  help: {
+    name: '',
+    aliases: [''] || '',
+  },
+  run: async function(<client>, <message>, args){
+    //comando...
+  }
+}
+//name = nome do comando
+//aliases = alternativas de uso do comando.
+//as aliases podem ser tanto uma Array ou uma String, somente.
