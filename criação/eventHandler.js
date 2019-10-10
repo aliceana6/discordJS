d48fb4b4a05e2b99a986ba11841009cd5ebfe171
Enersh <56371070+aliceana6@users.noreply.github.com>
2019-10-09 22:37:22 -0300

    Update eventHandler.js

--- criação/eventHandler.js
+++ criação/eventHandler.js
@@ -8,7 +8,7 @@ async function event(local){ //para executar utilize event('pasta')
   fs.readdir(local, (e, f)=>{ //lê a pasta
     f.forEach(i => { //para cada arquivo na pasta
       const evento = require(`${local}/${i}`) //procura o arquivo
-      <client>.on(i.split('.')[0], (...args) => evento.run(...args)) //executa o evento usando a function run() definida lá.
+      <client>.on(i.split('.')[0], (...args) => evento.run(<client>, ...args)) //executa o evento usando a function run() definida lá.
     })
   })
 }
